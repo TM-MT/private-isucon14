ef2db5eb9db9dee7d8e93b4e9354a346161a1a3d
TM-MT <tm-east@outlook.com>
2025-01-13 22:10:22 +0900

    検索条件を改善

--- webapp/rust/src/internal_handlers.rs
+++ webapp/rust/src/internal_handlers.rs
@@ -1,7 +1,7 @@
 use axum::extract::State;
 use axum::http::StatusCode;
 
-use crate::models::{Chair, Ride};
+use crate::models::Ride;
 use crate::{knn, AppState, Error};
 
 pub fn internal_routes() -> axum::Router<AppState> {
@@ -24,76 +24,90 @@ async fn internal_get_matching(
         return Ok(StatusCode::NO_CONTENT);
     };
 
-    for _ in 0..10 {
-        // おおよそ近いものを持ってくる
-        let Some(matched): Option<Chair> = ({
-            let m: Option<Chair> = sqlx::query_as(
-                r#"
-                SELECT
-                    c.*
-                FROM chairs c
-                INNER JOIN chair_total_distance ctd ON c.id=ctd.chair_id
-                WHERE
-                    ctd.hash = ?
-                    AND c.is_active = TRUE
-                ORDER BY RAND()
-                LIMIT 1"#,
-            )
-            .bind(knn::coord_to_hash(
-                ride.pickup_latitude,
-                ride.pickup_longitude,
-            ))
-            .fetch_optional(&pool)
-            .await?;
-
-            match m {
-                Some(_) => m,
-                None => {
-                    tracing::info!(
-                        "No suitable chair Found for ride ({:#?}, {:#?}). Using random instead.",
-                        ride.pickup_latitude,
-                        ride.pickup_longitude
-                    );
-                    sqlx::query_as(
-                        r#"
-                        SELECT
-                            c.*
-                        FROM chairs c
-                        INNER JOIN chair_total_distance ctd ON c.id=ctd.chair_id
-                        WHERE
-                            ctd.zone = ?
-                            AND c.is_active = TRUE
-                        ORDER BY RAND()
-                        LIMIT 1;
-                        "#,
+    let mut match_candidates: Vec<(String,)> = sqlx::query_as(
+        r#"
+        SELECT
+            c.id
+        FROM chairs c
+        INNER JOIN chair_total_distance ctd ON c.id=ctd.chair_id
+        WHERE
+            ctd.hash = ?
+            AND c.is_active = TRUE
+            AND (SELECT 
+                    COUNT(*) = 0 
+                FROM (
+                    SELECT 
+                        COUNT(chair_sent_at) = 6 AS completed 
+                    FROM ride_statuses 
+                    WHERE ride_id IN (
+                        SELECT id FROM rides WHERE chair_id = c.id
                     )
-                    .bind(knn::coord_to_zone(
-                        ride.pickup_latitude,
-                        ride.pickup_longitude,
-                    ))
-                    .fetch_optional(&pool)
-                    .await?
-                }
-            }
-        }) else {
-            return Ok(StatusCode::NO_CONTENT);
-        };
+                    GROUP BY ride_id
+                ) is_completed 
+                WHERE completed = FALSE)
+        ORDER BY (ABS(ctd.last_latitude - ?) + ABS(ctd.last_longitude - ?)) ASC
+        LIMIT 1"#,
+    )
+    .bind(knn::coord_to_hash(
+        ride.pickup_latitude,
+        ride.pickup_longitude,
+    ))
+    .bind(ride.pickup_latitude)
+    .bind(ride.pickup_longitude)
+    .fetch_all(&pool)
+    .await?;
 
-        let empty: bool = sqlx::query_scalar(
-            "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE",
+    if match_candidates.is_empty() {
+        tracing::info!(
+            "No suitable chair Found for ride ({:#?}, {:#?}). Using random instead.",
+            ride.pickup_latitude,
+            ride.pickup_longitude
+        );
+        match_candidates = sqlx::query_as(
+            r#"
+            SELECT
+                c.*
+            FROM chairs c
+            INNER JOIN chair_total_distance ctd ON c.id=ctd.chair_id
+            WHERE
+                ctd.zone = ?
+                AND c.is_active = TRUE
+                AND (SELECT 
+                        COUNT(*) = 0 
+                    FROM (
+                        SELECT 
+                            COUNT(chair_sent_at) = 6 AS completed 
+                        FROM ride_statuses 
+                        WHERE ride_id IN (
+                            SELECT id FROM rides WHERE chair_id = c.id
+                        )
+                        GROUP BY ride_id
+                    ) is_completed 
+                    WHERE completed = FALSE)
+            ORDER BY RAND()
+            LIMIT 1;
+            "#,
         )
-        .bind(&matched.id)
-        .fetch_one(&pool)
+        .bind(knn::coord_to_zone(
+            ride.pickup_latitude,
+            ride.pickup_longitude,
+        ))
+        .fetch_all(&pool)
         .await?;
+    }
 
-        if empty {
-            sqlx::query("UPDATE rides SET chair_id = ? WHERE id = ?")
-                .bind(matched.id)
-                .bind(ride.id)
-                .execute(&pool)
-                .await?;
-            break;
-        }
+    if let Some((matched,)) = match_candidates.into_iter().next() {
+        sqlx::query("UPDATE rides SET chair_id = ? WHERE id = ?")
+            .bind(matched)
+            .bind(ride.id)
+            .execute(&pool)
+            .await?;
+    } else {
+        tracing::info!(
+            "No match found for ride ({:?}, {:?})",
+            ride.pickup_latitude,
+            ride.pickup_longitude
+        );
     }
 
     Ok(StatusCode::NO_CONTENT)
